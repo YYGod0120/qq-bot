86199dc779d47b541d640114f9e3bce856302b7e
YYGod <2569630663@qq.com>
2023-10-18 12:56:36 +0800

    fix: api签名搞定

--- dist/bot.js
+++ dist/bot.js
@@ -2,20 +2,8 @@ import { groupID, masterID } from "./package/config.js";
 import { login } from "./package/login.js";
 import { createClient } from "icqq";
 const client = createClient({
-    platform: 3,
-    sign_api_addr: "http://qi.yize.site/sign?key=939151520 ",
+    platform: 1,
+    sign_api_addr: "http://127.0.0.1:8090/sign?key=114514",
 });
 login({ account: 2907724711, password: "yangg308911" });
-//监听群聊消息
-client.on("system.online", () => {
-    console.log("账号登录成功");
-});
-client.on("system.online", () => {
-    //运用sendPrivateMsg实现发送私聊消息
-    client.sendPrivateMsg(masterID, "Hello World");
-    //监听所有私聊消息
-    client.on("message.private", (event) => {
-        console.log(`收到私聊消息来自${event.sender.user_id}, 文本内容为:${event.raw_message}`);
-    });
-});
 //# sourceMappingURL=bot.js.map
--- package/login.ts
+++ package/login.ts
@@ -1,5 +1,6 @@
 import { createClient } from "icqq";
-
+import { groupID, masterID } from "./config.js";
+import { segment } from "icqq";
 type Account = {
   account: number;
   password: string;
@@ -11,8 +12,8 @@ type Account = {
  */
 export function login(account: Account) {
   const client = createClient({
-    platform: 3,
-    sign_api_addr: "http://qi.yize.site/sign?key=939151520 ",
+    platform: 1,
+    sign_api_addr: "http://127.0.0.1:8090/sign?key=114514",
   });
   const userAccount = account.account;
   const password = account.password;
@@ -46,4 +47,29 @@ export function login(account: Account) {
     });
   });
   client.login(userAccount, password);
+  //监听群聊消息
+  client.on("system.online", () => {
+    //运用sendPrivateMsg实现发送私聊消息
+    client.sendGroupMsg(groupID, "fuck all 我tm能用了");
+
+    //监听所有私聊消息
+    client.on("message.private", (event) => {
+      client.sendPrivateMsg(masterID, "123456");
+      console.log(
+        `收到私聊消息来自${event.sender.user_id}, 文本内容为:${event.raw_message}`
+      );
+    });
+
+    //监听群聊消息
+    client.on("message.group", (event) => {
+      if (event.atme) {
+        client.sendGroupMsg(groupID, [
+          "小心我肘击你",
+          segment.at(event.sender.user_id),
+        ]);
+      }
+    });
+  });
 }
+
+console.log(111);
